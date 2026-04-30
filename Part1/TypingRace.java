b6df3a9d723c250b748b59d223170f39bb964ca1
FaizaanAk <faizaanakhtarw@gmail.com>
2026-04-30 02:50:03 +0100

    Added the winner's final accuracy at the end of the race

--- Part1/TypingRace.java
+++ Part1/TypingRace.java
@@ -123,6 +123,7 @@ public class TypingRace
         
         System.out.println();
         System.out.println("And the winner is... " + winner.getName() + "!");
+        System.out.println("Final accuracy: " +  winner.getAccuracy());
     }
 
     /**
